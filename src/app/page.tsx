1d6e06eeac1bfc8b86fc5e873eba14eadb1da81d
Murat <muratsat@users.noreply.github.com>
2025-09-18 16:47:01 +0600

    hide no prefetch link from viewport

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b">
+      <div className="h-screen">Scroll down</div>
+
       <Link href="/dashboard" className="text-2xl font-bold underline">
         Dashboard (Prefetch)
       </Link>
-      <div className="h-screen">Scroll down</div>
-
       <Link
         href="/dashboard-no-prefetch"
         className="text-2xl font-bold underline"
